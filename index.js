75ff4af344674f92e3f0eb8f8bb0d1c647b5d972
synnheal <103332501+synnheal@users.noreply.github.com>
2025-10-03 09:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-const { Client, GatewayIntentBits, Partials, Events, InteractionType } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Events } = require('discord.js');
 const token = process.env.TOKEN;
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ],
   partials: [Partials.Channel],
 });
+
+const prefix = '!';
 
 client.once(Events.ClientReady, () => {
   console.log(`✅ Bot connecté en tant que ${client.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-  if (interaction.type !== InteractionType.ApplicationCommand) return;
+client.on(Events.MessageCreate, async message => {
+  if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-  if (interaction.commandName === 'blague') {
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  // !blague
+  if (command === 'blague') {
     const blagues = [
       'Pourquoi les canards ont-ils autant de plumes ? Pour couvrir leur derrière.',
       'C’est l’histoire d’un pingouin qui respirait par les fesses. Un jour il s’assoit et il meurt.',
       'J’ai une blague sur les chaises, mais elle est pliante.'
     ];
     const blague = blagues[Math.floor(Math.random() * blagues.length)];
-    await interaction.reply(blague);
+    message.reply(blague);
   }
 
-  if (interaction.commandName === 'insulte') {
+  // !insulte
+  else if (command === 'insulte') {
     const insultes = [
       'T’es plus inutile qu’un chargeur Apple sans fil.',
       'Même Clippy te trouve inutile.',
       'T’as la répartie d’un ticket de caisse.'
     ];
     const insulte = insultes[Math.floor(Math.random() * insultes.length)];
-    await interaction.reply(insulte);
+    message.reply(insulte);
   }
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('🏓 pong');
+  // !ping
+  else if (command === 'ping') {
+    message.reply('🏓 pong');
   }
 });
 
